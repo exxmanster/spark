4dd2809b801e06d05331880604014fa82275401a
exxmanster <exxmanster@gmail.com>
2022-01-18 19:44:03 +0200

    update instances

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -5,6 +5,7 @@ module "ec2-web1" {
   key_name  = aws_key_pair.ssh-key-from-bastion.key_name
   tag_name  = "web1"
   tag_type  = "web"
+  tag_env   = "geen"
 }
 
 
@@ -16,10 +17,36 @@ module "ec2-web2" {
   key_name  = aws_key_pair.ssh-key-from-bastion.key_name
   tag_name  = "web2"
   tag_type  = "web"
+  tag_env   = "blue"
+
 }
 
 
-module "ec2-db1" {
+module "ec2-php1" {
+  source = ".//modules/ec2"
+
+  subnet_id = module.vpc.private_subnet1_id
+  sg_ids    = [module.vpc.http_sg, module.vpc.ssh_sg]
+  key_name  = aws_key_pair.ssh-key-from-bastion.key_name
+  tag_name  = "php1"
+  tag_type  = "php"
+  tag_env   = "geen"
+
+}
+
+module "ec2-php2" {
+  source = ".//modules/ec2"
+
+  subnet_id = module.vpc.private_subnet2_id
+  sg_ids    = [module.vpc.http_sg, module.vpc.ssh_sg]
+  key_name  = aws_key_pair.ssh-key-from-bastion.key_name
+  tag_name  = "php2"
+  tag_type  = "php"
+  tag_env   = "blue"
+
+}
+
+module "ec2-db" {
   source = ".//modules/ec2"
 
   subnet_id = module.vpc.private_subnet1_id
@@ -27,5 +54,5 @@ module "ec2-db1" {
   key_name  = aws_key_pair.ssh-key-from-bastion.key_name
   tag_name  = "db1"
   tag_type  = "db"
+  tag_env   = "share"
 }
-
