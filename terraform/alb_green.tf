25193e2c99768a68cff2810942e03085653223f6
exxmanster <exxmanster@gmail.com>
2022-01-19 11:55:30 +0200

    Add a green ALB

--- terraform/alb_green.tf
+++ terraform/alb_green.tf
@@ -1,61 +1,52 @@
-resource "aws_alb" "main" {
-  name               = "MainELBv2"
+resource "aws_alb" "green" {
+  name               = "GreenELBv2"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [module.vpc.http_sg, module.vpc.ssh_sg]
   subnets            = [module.vpc.public_subnet1_id, module.vpc.public_subnet2_id]
 
   tags = {
-    Name = "Main elb"
+    Name = "GreenALB"
   }
 }
 
-resource "aws_alb_target_group" "web" {
+resource "aws_alb_target_group" "web_green" {
   name     = "WebTargetGroup"
   port     = 80
   protocol = "HTTP"
   vpc_id   = module.vpc.vpc_id
   tags = {
-    Name = "Web TG"
+    Name = "Web Green TG"
   }
 }
 
-resource "aws_alb_target_group" "php" {
+resource "aws_alb_target_group" "php_green" {
   name     = "PhpTargetGroup"
   port     = 80
   protocol = "HTTP"
   vpc_id   = module.vpc.vpc_id
   tags = {
-    Name = "php TG"
+    Name = "PHP Green TG"
   }
 }
 
 resource "aws_alb_target_group_attachment" "web1" {
 
-  target_group_arn = aws_alb_target_group.web.arn
+  target_group_arn = aws_alb_target_group.web_green.arn
   target_id        = module.ec2-web1.instance[0]
   port             = 80
 }
-resource "aws_alb_target_group_attachment" "web2" {
-  target_group_arn = aws_alb_target_group.web.arn
-  target_id        = module.ec2-web2.instance[0]
-  port             = 80
-}
+
 
 resource "aws_alb_target_group_attachment" "php1" {
-  target_group_arn = aws_alb_target_group.php.arn
+  target_group_arn = aws_alb_target_group.php_green.arn
   target_id        = module.ec2-php1.instance[0]
   port             = 80
 }
 
-resource "aws_alb_target_group_attachment" "php2" {
-  target_group_arn = aws_alb_target_group.php.arn
-  target_id        = module.ec2-php2.instance[0]
-  port             = 80
-}
 
-resource "aws_alb_listener" "web" {
-  load_balancer_arn = aws_alb.main.arn
+resource "aws_alb_listener" "web_green" {
+  load_balancer_arn = aws_alb.green.arn
   port              = "80"
   protocol          = "HTTP"
 
@@ -66,7 +57,7 @@ resource "aws_alb_listener" "web" {
 }
 
 
-resource "aws_alb_listener_rule" "db" {
+resource "aws_alb_listener_rule" "php_green" {
   listener_arn = aws_alb_listener.web.arn
   priority     = 99
 
@@ -84,7 +75,7 @@ resource "aws_alb_listener_rule" "db" {
 
 
 resource "aws_security_group" "alb_sg" {
-  name        = "ELBv2 Security group"
+  name        = "ELBv2 Grren Security group"
   description = "Allow all traffic to LB"
   vpc_id      = module.vpc.vpc_id
 
@@ -103,6 +94,6 @@ resource "aws_security_group" "alb_sg" {
   }
 
   tags = {
-    Name = "alb_sg"
+    Name = "alb_green_sg"
   }
 }
