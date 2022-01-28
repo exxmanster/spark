115272264018052bff0244fd2786e989b2946a4b
exxmanster <exxmanster@gmail.com>
2022-01-28 17:13:00 +0200

    db instanse fix

--- terraform/db_instance.tf
+++ terraform/db_instance.tf
@@ -1,6 +1,6 @@
 module "ec2-db" {
   source = ".//modules/ec2"
-  ami_id    = data.aws_ami.redhat.id
+  ami_id    = data.aws_ami.ama2.id
   subnet_id = module.vpc.private_subnet_db_id
   sg_ids    = [module.vpc.db_sg, module.vpc.ssh_sg]
   key_name  = aws_key_pair.ssh-key-from-bastion.key_name
